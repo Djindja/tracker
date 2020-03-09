4d3e43324e25d397842461c6b1bce690fcaaf229
Stefan <stefansdjindjic@gmail.com>
2020-03-09 23:28:41 +0100

    part2 - finished

--- server/src/app.js
+++ server/src/app.js
@@ -3,23 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const {sequelize} = require('./models');
+const config = require('./config/config');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: `Hello Stefan!`,
-  });
-});
+// app.get('/status', (req, res) => {
+//   res.send({
+//     message: `Hello Stefan!`,
+//   });
+// });
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered!`,
-  });
-});
+require('./routes')(app)
 
 sequelize.sync()
   .then(() => {
